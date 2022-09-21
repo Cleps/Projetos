a7b1e92e464f12963f306627fe2d99cd7f97da6b
Clécio.e <104847424+Cleps@users.noreply.github.com>
2022-09-21 08:07:55 -0500

    update

--- Med_hub/main.py
+++ Med_hub/main.py
@@ -12,8 +12,8 @@ class Tela_loguin:
             
         self.lbl_img = tk.Label(self.janela, bg='pale green', width=23, height=10)
         self.lbl_img.place(rely=.1, relx=.28)
-        self.btn_user = tk.Button(self.janela, text='Sou gerente',  font=("calibre",10,'bold') ,width=15, height=2, command=self.manager)
-        self.btn_user.place(rely=.5, relx=.33)
+        self.btn_gen = tk.Button(self.janela, text='Sou gerente',  font=("calibre",10,'bold') ,width=15, height=2, command=self.manager)
+        self.btn_gen.place(rely=.5, relx=.33)
         self.btn_user = tk.Button(self.janela, text='Sou cliente',  font=("calibre",10,'bold'), width=15, height=2)
         self.btn_user.place(rely=.6, relx=.33)
         
@@ -31,12 +31,22 @@ class Tela_loguin:
         #self.top_login.grab_set()
         self.lbl_img = tk.Label(self.janela, bg='pale green', width=23, height=10)
         self.lbl_img.place(rely=.1, relx=.28)
+        self.btn_voltar = tk.Button(self.janela, text='Voltar',command=self.voltar_janela).place(relx=.05, rely=.02)
         self.lbl = tk.Label(self.janela, text='Senha de administrador:', font=("calibre",10,'bold'), bg='grey')
         self.lbl.place(relx=.27, rely=.45)
         self.ent = tk.Entry(self.janela, width=24, show='*')
         self.ent.place(relx=.29,rely=.5)
         self.btn = tk.Button(self.janela, text='Login',width=10, font=("calibre",10,'bold'))
         self.btn.place(relx=.38,rely=.55)
-
+    
+    def voltar_janela(self):
+        for widget in self.janela.winfo_children():
+            widget.destroy()
+        self.lbl_img = tk.Label(self.janela, bg='pale green', width=23, height=10)
+        self.lbl_img.place(rely=.1, relx=.28)
+        self.btn_gen = tk.Button(self.janela, text='Sou gerente',  font=("calibre",10,'bold') ,width=15, height=2, command=self.manager)
+        self.btn_gen.place(rely=.5, relx=.33)
+        self.btn_user = tk.Button(self.janela, text='Sou cliente',  font=("calibre",10,'bold'), width=15, height=2)
+        self.btn_user.place(rely=.6, relx=.33)
 
 Tela_loguin()
